ec0d57cce52bd859e06fdbd9af107b7dfb91f93b
Leexiaop <282191344@qq.com>
2023-08-28 14:09:41 +0800

    Updates

---
+++ docs__program__program.md.2e03db85.async.js
@@ -0,0 +1,36 @@
+"use strict";(self.webpackChunkibadgers=self.webpackChunkibadgers||[]).push([[140],{17291:function(u,a,s){s.r(a);var r=s(2143),d=s(93359),h=s(15371),v=s(31446),o=s(59973),c=s(96057),x=s(39988),n=s(57199),_=s(67294),l=s(85893);function i(){var t=(0,n.eL)(),e=t.texts;return(0,l.jsx)(n.dY,{children:(0,l.jsx)(l.Fragment,{children:(0,l.jsxs)("div",{className:"markdown",children:[(0,l.jsxs)("ul",{className:"program",children:[e[0].value,(0,l.jsxs)("li",{children:[e[1].value,(0,l.jsxs)("a",{href:"https://tinypng.com/",children:[e[2].value,(0,l.jsx)("img",{src:"http://leexiaop.github.io/static/ibadgers/program/shrunk.png"}),e[3].value,(0,l.jsx)("span",{children:e[4].value}),e[5].value]}),e[6].value]}),e[7].value,(0,l.jsxs)("li",{children:[e[8].value,(0,l.jsxs)("a",{href:"https://github.com/dragonir/3d",children:[e[9].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[10].value}),e[11].value]}),e[12].value]}),e[13].value,(0,l.jsxs)("li",{children:[e[14].value,(0,l.jsxs)("a",{href:"https://91huajian.cn/",children:[e[15].value,(0,l.jsx)("img",{src:"https://91huajian.cn/static/images/91huajian-caise-5c86eb75.svg"}),e[16].value,(0,l.jsx)("span",{children:e[17].value}),e[18].value]}),e[19].value]}),e[20].value,(0,l.jsxs)("li",{children:[e[21].value,(0,l.jsxs)("a",{href:"https://github.com/brunosimon/my-room-in-3d",children:[e[22].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[23].value}),e[24].value]}),e[25].value]}),e[26].value,(0,l.jsxs)("li",{children:[e[27].value,(0,l.jsxs)("a",{href:"https://github.com/nagix/mini-tokyo-3d",children:[e[28].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[29].value}),e[30].value]}),e[31].value]}),e[32].value,(0,l.jsxs)("li",{children:[e[33].value,(0,l.jsxs)("a",{href:"https://codeleilei.gitee.io/markdown2pdf/#/home",children:[e[34].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[35].value}),e[36].value]}),e[37].value]}),e[38].value,(0,l.jsxs)("li",{children:[e[39].value,(0,l.jsxs)("a",{href:"https://www.mujicv.com/",children:[e[40].value,(0,l.jsx)("img",{src:"https://cdn.mdedit.online/static/img/muji-ai-post.jpeg!webp"}),e[41].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[42].value}),e[43].value]}),e[44].value]}),e[45].value,(0,l.jsxs)("li",{children:[e[46].value,(0,l.jsxs)("a",{href:"https://github.com/w3reality/three-geo",children:[e[47].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[48].value}),e[49].value]}),e[50].value]}),e[51].value,(0,l.jsxs)("li",{children:[e[52].value,(0,l.jsxs)("a",{href:"https://github.com/brunosimon/folio-2019",children:[e[53].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[54].value}),e[55].value]}),e[56].value]}),e[57].value,(0,l.jsxs)("li",{children:[e[58].value,(0,l.jsxs)("a",{href:"https://rxresu.me/",children:[e[59].value,(0,l.jsx)("img",{src:"https://rxresu.me/logo/dark.svg"}),e[60].value,(0,l.jsx)("span",{children:e[61].value}),e[62].value]}),e[63].value]}),e[64].value,(0,l.jsxs)("li",{children:[e[65].value,(0,l.jsxs)("a",{href:"https://github.com/0xFloyd/Portfolio_2020",children:[e[66].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[67].value}),e[68].value]}),e[69].value]}),e[70].value,(0,l.jsxs)("li",{children:[e[71].value,(0,l.jsxs)("a",{href:"https://github.com/cesiumlab/2019-nCoV-3d",children:[e[72].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[73].value}),e[74].value]}),e[75].value]}),e[76].value,(0,l.jsxs)("li",{children:[e[77].value,(0,l.jsxs)("a",{href:"https://www.open-resume.com/",children:[e[78].value,(0,l.jsx)("img",{src:"https://www.open-resume.com/_next/static/media/logo.90ac2b67.svg"}),e[79].value,(0,l.jsx)("span",{children:e[80].value}),e[81].value]}),e[82].value]}),e[83].value,(0,l.jsxs)("li",{children:[e[84].value,(0,l.jsxs)("a",{href:"https://glbxz.com/glbxz/editor/index.html",children:[e[85].value,(0,l.jsx)("img",{src:"https://glbxz.com/wp-content/uploads/2023/03/1679740176-d02a42d9cb3dec9.png"}),e[86].value,(0,l.jsx)("span",{children:e[87].value}),e[88].value]}),e[89].value]}),e[90].value,(0,l.jsxs)("li",{children:[e[91].value,(0,l.jsxs)("a",{href:"https://github.com/mtsee/vr-hall",children:[e[92].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[93].value}),e[94].value]}),e[95].value]}),e[96].value,(0,l.jsxs)("li",{children:[e[97].value,(0,l.jsxs)("a",{href:"https://github.com/ViciousFish/buzzwords",children:[e[98].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[99].value}),e[100].value]}),e[101].value]}),e[102].value,(0,l.jsxs)("li",{children:[e[103].value,(0,l.jsxs)("a",{href:"https://github.com/celestiary/web",children:[e[104].value,(0,l.jsx)("span",{style:{fontWeight:"600",fontSize:"24px"},children:e[105].value}),e[106].value]}),e[107].value]}),e[108].value,(0,l.jsxs)("li",{children:[e[109].value,(0,l.jsxs)("a",{href:"https://glbxz.com/",children:[e[110].value,(0,l.jsx)("img",{src:"https://glbxz.com/wp-content/uploads/2023/03/1679740176-d02a42d9cb3dec9.png"}),e[111].value,(0,l.jsx)("span",{children:e[112].value}),e[113].value]}),e[114].value]})]}),(0,l.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+    ul.program {
+        margin: 0;
+        padding: 0;
+        display: flex;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+    }
+    ul.program > li {
+        width: 20%;
+        margin: 8px 16px;
+        border-radius: 8px;
+        list-style: none;
+        box-shadow: 0 0 10px 0px rgba(0,0,0,0.5);
+        padding: 8px 0;
+    }
+    ul.program > li > a {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+    }
+    ul.program > li > a:hover {
+        text-decoration: none;
+        opacity: 1;
+    }
+    ul.program > li > a > img {
+        display: block;
+        width: 5rem;
+        height: 5rem;
+        border-radius: 50%;
+    }
+`}})]})})})}a.default=i}}]);
